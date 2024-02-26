575b4e1532502d0fe99f7503a9cc03ae74917703
Erick Mireles <emireles.rosas@gmail.com>
2024-02-26 16:05:13 -0600

    Add scrollToggle and mirrorClick functionality

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-alert("It works!");
+// Set the Footer rights Year automatically
+
+//When the DOM is ready
+document.addEventListener("DOMContentLoaded", function () {
+  // Setting the current year in the footer
+  //Get the span element
+  const footerYear = document.querySelector("[footer='year']");
+  //Get Current year
+  const year = new Date().getFullYear();
+  //Set the span element's text to be the year
+  footerYear.textContent = year;
+
+  // Scroll disable toggle function
+  let scrollDisable = false;
+  function toggleScroll() {
+    if (scrollDisable) {
+      document.body.style.overflow = "auto";
+      scrollDisable = false;
+    } else {
+      document.body.style.overflow = "hidden";
+      scrollDisable = true;
+    }
+  }
+  // Make the scroll toggle when the button is clicked
+  const toggleScrollElement = document.querySelector("[scroll='toggle']");
+  toggleScrollElement.addEventListener("click", toggleScroll);
+
+  // Mirror a click onto another element
+  const mirrorClickTrigger = document.querySelector("[mirror-click='trigger']");
+  const mirrorClickTarget = document.querySelector("[mirror-click='target']");
+  mirrorClickTrigger.addEventListener("click", () => {
+    mirrorClickTarget.click();
+  });
+});
